400ee2bf3d13f1bb7b4cc54bfe9ddb609fbdd26f
fatihkosecakmak <155267346+fatihkosecakmak@users.noreply.github.com>
2024-06-22 17:59:05 +0300

    assets added to the project

--- pamukkale_app/lib/main.dart
+++ pamukkale_app/lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pamukkale_app/ui/cubits/home_cubit.dart';
+import 'package:pamukkale_app/ui/cubits/login_cubit.dart';
+import 'package:pamukkale_app/ui/cubits/my_profile_cubit.dart';
+import 'package:pamukkale_app/ui/cubits/signup_cubit.dart';
+import 'package:pamukkale_app/ui/cubits/trip_detail_cubit.dart';
+import 'package:pamukkale_app/ui/cubits/trips_cubit.dart';
+import 'package:pamukkale_app/ui/views/home.dart';
 import 'package:pamukkale_app/ui/views/login.dart';
+import 'package:pamukkale_app/ui/views/my_profile.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,13 +22,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => HomeCubit(),),
+        BlocProvider(create: (context) => LoginCubit(),),
+        BlocProvider(create: (context) => MyProfileCubit(),),
+        BlocProvider(create: (context) => SignUpCubit(),),
+        BlocProvider(create: (context) => TripDetailCubit(),),
+        BlocProvider(create: (context) => TripCubit(),),
+      ],
+      child: MaterialApp(debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Home(),
       ),
-      home: const Login(),
     );
   }
 }
